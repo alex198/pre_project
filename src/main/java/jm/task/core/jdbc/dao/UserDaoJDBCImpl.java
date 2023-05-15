9a4d1706a8fb76946bfee6be092add765ca0e567
alex198 <abykovskih@gmail.com>
2023-05-15 23:01:00 +0300

    1_1_4 перенос connection в трай с ресурсами

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,41 +9,35 @@ import java.util.List;
 
 public class UserDaoJDBCImpl extends Util implements UserDao {
 
-    Connection connection = getConnection();
     public UserDaoJDBCImpl() {
 
     }
     @Override
     public void createUsersTable() throws SQLException {
         String sql = "CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45), lastname VARCHAR(45), age TINYINT)";
-        try (Statement statement = connection.createStatement()) {
+        try (Connection connection = getConnection();
+              Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
         }
     }
     @Override
     public void dropUsersTable() throws SQLException {
         String sql = "DROP TABLE IF EXISTS users";
-        try (Statement statement = connection.createStatement()) {
+        try ( Connection connection = getConnection();
+              Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
         }
     }
     @Override
     public void saveUser(String name, String lastName, byte age) throws SQLException {
         String sql = "INSERT INTO users (name, lastname, age) VALUES(?, ?, ?)";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try ( Connection connection = getConnection();
+              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
@@ -52,25 +46,18 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
         }
     }
     @Override
     public void removeUserById(long id) throws SQLException {
         String sql = "DELETE FROM users WHERE id=?";
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql))  {
+        try ( Connection connection = getConnection();
+              PreparedStatement preparedStatement = connection.prepareStatement(sql))  {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
         }
     }
     @Override
@@ -78,7 +65,8 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
         List<User> userList = new ArrayList<>();
         String sql = "SELECT name, lastname, age FROM users";
 
-        try (Statement statement = connection.createStatement()) {
+        try ( Connection connection = getConnection();
+              Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(sql);
 
             while(resultSet.next()) {
@@ -90,24 +78,17 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
         }
         return userList;
     }
     @Override
     public void cleanUsersTable() throws SQLException {
         String sql = "DELETE FROM users";
-        try (Statement statement = connection.createStatement()) {
+        try ( Connection connection = getConnection();
+              Statement statement = connection.createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
         }
     }
 }
